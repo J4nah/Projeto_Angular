7ded79a724ca030886c7b19b15591064e070bfd7
Janaína Gomes <jana.gomes90@hotmail.com>
2024-10-07 22:49:26 -0300

    up

--- Projeto_Angular/src/app/principal/principal.component.ts
+++ Projeto_Angular/src/app/principal/principal.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
-  styleUrl: './principal.component.scss'
+  styleUrls: ['./principal.component.scss']
 })
 export class PrincipalComponent {
+  closeResult = ''; 
 
+  constructor(private offcanvasService: NgbOffcanvas) {}
+
+ 
+  open(content: any) {
+    this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;  
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`; 
+      }
+    );
+  }
+
+
+  private getDismissReason(reason: any): string {
+    if (reason === OffcanvasDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === OffcanvasDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on the backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
